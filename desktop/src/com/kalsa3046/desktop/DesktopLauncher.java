b0e120794da885dd5d551c69b5cfd806b027050d
kalsa3046 <kalsa3046@HRH-IC0027340.wrdsb.ca>
2018-11-26 08:53:31 -0500

    gdx lesson

---
+++ desktop/src/com/kalsa3046/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kalsa3046.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kalsa3046.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
